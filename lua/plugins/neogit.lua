fdf867a484c37bdd02a48da66104dc0494d26b22
Filippo Ragazzo <filipporagazzo89@gmail.com>
2024-12-19 14:10:31 +0100

    Add Neogit keymaps

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -6,6 +6,12 @@ return {
       'sindrets/diffview.nvim',
       'nvim-telescope/telescope.nvim',
     },
-    config = true,
+    config = function()
+      vim.keymap.set('n', '<leader>gg', ':Neogit<CR>', { desc = 'Neo[G]it' })
+      vim.keymap.set('n', '<leader>gl', ':Neogit log<CR>', { desc = 'Neo[G]it [L]og' })
+      vim.keymap.set('n', '<leader>gd', ':DiffviewOpen<CR>', { desc = '[G]it [D]iffview open' })
+      vim.keymap.set('n', '<leader>gc', ':DiffviewClose<CR>', { desc = '[G]it [D]iffview close' })
+      vim.keymap.set('n', '<leader>gD', ':DiffviewOpen master<CR>', { desc = '[G]it [D]iffview open master' })
+    end,
   },
 }
