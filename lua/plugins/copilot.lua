83d86d2814cbf281c1b701b7ef18f3b231754adb
Filippo <filipporagazzo89@gmail.com>
2025-01-06 15:22:36 +0100

    Change copilot configuration

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,3 +1,15 @@
 return {
   'github/copilot.vim', -- GitHub Copilot
+  config = function()
+    -- Disable by default
+    vim.g.copilot_filetypes = {
+      ['*'] = false,
+    }
+
+    -- Explicitly request for copilot suggestions on Ctrl-Enter
+    vim.keymap.set('i', '<C-CR>', '<Plug>(copilot-suggest)')
+
+    vim.keymap.set('n', '<leader>ce', ':Copilot enable<CR>', { desc = '[C]opilot [E]nable' })
+    vim.keymap.set('n', '<leader>cd', ':Copilot disable<CR>', { desc = '[C]opilot [D]isable' })
+  end,
 }
