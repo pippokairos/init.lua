ba8ae88a59ec690cd8c40261e5d32477d86e4d70
Filippo <filipporagazzo89@gmail.com>
2025-03-19 10:46:45 +0100

    Use copilot.lua and fix LSP config

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,15 +1,33 @@
+-- return {
+--   'github/copilot.vim', -- GitHub Copilot
+--   config = function()
+--     -- Disable by default
+--     vim.g.copilot_filetypes = {
+--       ['*'] = false,
+--     }
+--
+--     -- Explicitly request for copilot suggestions on Ctrl-Enter
+--     vim.keymap.set('i', '<C-CR>', '<Plug>(copilot-suggest)')
+--
+--     vim.keymap.set('n', '<leader>ce', ':Copilot enable<CR>', { desc = '[C]opilot [E]nable' })
+--     vim.keymap.set('n', '<leader>cd', ':Copilot disable<CR>', { desc = '[C]opilot [D]isable' })
+--   end,
+-- }
+
 return {
-  'github/copilot.vim', -- GitHub Copilot
+  'zbirenbaum/copilot.lua',
   config = function()
-    -- Disable by default
-    vim.g.copilot_filetypes = {
-      ['*'] = false,
+    require('copilot').setup {
+      suggestion = {
+        enabled = true,
+        auto_trigger = false,
+        keymap = {
+          accept = '<Tab>',
+          accept_word = false,
+          accept_line = false,
+          next = '<C-CR>',
+        },
+      },
     }
-
-    -- Explicitly request for copilot suggestions on Ctrl-Enter
-    vim.keymap.set('i', '<C-CR>', '<Plug>(copilot-suggest)')
-
-    vim.keymap.set('n', '<leader>ce', ':Copilot enable<CR>', { desc = '[C]opilot [E]nable' })
-    vim.keymap.set('n', '<leader>cd', ':Copilot disable<CR>', { desc = '[C]opilot [D]isable' })
   end,
 }
