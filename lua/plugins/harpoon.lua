172d245d35d4186a10a0e4390529ab25797e03e6
Filippo <filipporagazzo89@gmail.com>
2025-04-18 16:32:09 +0200

    Move lualine to its own configuration file

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -32,20 +32,4 @@ return {
       end, { desc = 'Harpoon select 4' })
     end,
   },
-
-  {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'abeldekat/harpoonline', version = '*' },
-    config = function()
-      local Harpoonline = require 'harpoonline'
-      Harpoonline.setup {
-        on_update = function()
-          require('lualine').refresh()
-        end,
-      }
-
-      local lualine_c = { Harpoonline.format, 'filename' }
-      require('lualine').setup { sections = { lualine_c = lualine_c } }
-    end,
-  },
 }
---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,43 @@
+return {
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons', 'abeldekat/harpoonline' },
+    opts = function()
+      local harpoon_component
+      local harpoonline_ok, Harpoonline = pcall(require, 'harpoonline')
+      if harpoonline_ok then
+        Harpoonline.setup()
+        harpoon_component = Harpoonline.format
+      else
+        vim.notify('Harpoonline not found or failed to load for Lualine', vim.log.levels.WARN)
+      end
+
+      return {
+        options = {
+          icons_enabled = vim.g.have_nerd_font,
+          theme = 'catppuccin',
+          component_separators = { left = '', right = '' },
+          section_separators = { left = '', right = '' },
+          globalstatus = false,
+        },
+        sections = {
+          lualine_a = { 'mode' },
+          lualine_b = { 'branch', 'diff', 'diagnostics' },
+          lualine_c = { harpoon_component, 'filename' },
+          lualine_x = { 'encoding', 'fileformat', 'filetype' },
+          lualine_y = { 'progress' },
+          lualine_z = { 'location' },
+        },
+        inactive_sections = {
+          lualine_a = {},
+          lualine_b = {},
+          lualine_c = { 'filename' },
+          lualine_x = { 'location' },
+          lualine_y = {},
+          lualine_z = {},
+        },
+        extensions = { 'nvim-tree', 'trouble' },
+      }
+    end,
+  },
+}
