6a981d277656f0c085087c6912ad75ece50b3dae
Filippo <filipporagazzo89@gmail.com>
2026-07-16 15:53:50 +0200

    Update blink

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,13 +1,27 @@
 return {
   {
     'saghen/blink.cmp',
+    version = '1.*',
     dependencies = {
-      'L3MON4D3/LuaSnip',
       'rafamadriz/friendly-snippets',
     },
-    version = 'v0.*',
     opts = {
-      keymap = { preset = 'default' },
+      keymap = {
+        preset = 'default',
+        ['<Tab>'] = {
+          'snippet_forward',
+          function(_cmp)
+            local ok, suggestion = pcall(require, 'copilot.suggestion')
+            if not ok then return end
+            if suggestion.is_visible() then
+              suggestion.accept()
+            else
+              suggestion.next()
+            end
+            return true
+          end,
+        },
+      },
       appearance = {
         use_nvim_cmp_as_default = true,
         nerd_font_variant = 'mono',
